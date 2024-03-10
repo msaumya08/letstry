cde90cac7581ee6645d20504046fdf8c22dd7c9a
unknown <msaumyaa08@gmail.com git config --global user.email msaumyaa08@gmail.com>
2024-03-10 01:37:25 -0500

    added cpp file and para in letstry

---
+++ try.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=4;
+    double d=4.0;
+    string s="VSCODE";
+
+    int i2;
+    double d2;
+    string s2;
+
+    cin>>i2;
+    cin>>d2;
+    cin.get()
+    getline(cin,s2);
+
+    cout<<i+i2<<endl;
+    cout<<d+d2<<endl;
+    cout<<s+s2<<endl;
+
+    return 0;
+}
